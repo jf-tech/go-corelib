d08365b127b92e4fcec763cb563ebf568a728985
JF Technology <57098161+jf-tech@users.noreply.github.com>
2022-02-23 15:35:09 +1300

    add more maths helpers

--- maths/maths_test.go
+++ maths/maths_test.go
@@ -43,3 +43,71 @@ func TestMinMaxInt(t *testing.T) {
 		})
 	}
 }
+
+func TestAbsIntAndInt64(t *testing.T) {
+	tests := []struct {
+		name     string
+		in       int
+		expected int
+	}{
+		{
+			name:     "in > 0",
+			in:       1,
+			expected: 1,
+		},
+		{
+			name:     "in == 0",
+			in:       0,
+			expected: 0,
+		},
+		{
+			name:     "in < 0",
+			in:       -4,
+			expected: 4,
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			assert.Equal(t, test.expected, AbsInt(test.in))
+			assert.Equal(t, int64(test.expected), AbsI64(int64(test.in)))
+		})
+	}
+}
+
+func TestMinMaxI64(t *testing.T) {
+	tests := []struct {
+		name        string
+		x           int64
+		y           int64
+		expectedMin int64
+		expectedMax int64
+	}{
+		{
+			name:        "x less than y",
+			x:           1,
+			y:           2,
+			expectedMin: 1,
+			expectedMax: 2,
+		},
+		{
+			name:        "x greater than y",
+			x:           2,
+			y:           1,
+			expectedMin: 1,
+			expectedMax: 2,
+		},
+		{
+			name:        "x equal to y",
+			x:           2,
+			y:           2,
+			expectedMin: 2,
+			expectedMax: 2,
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			assert.Equal(t, test.expectedMin, MinI64(test.x, test.y))
+			assert.Equal(t, test.expectedMax, MaxI64(test.x, test.y))
+		})
+	}
+}
